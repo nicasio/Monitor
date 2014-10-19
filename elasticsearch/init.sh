a88f93d37410ef9236651eaeccda079b4f13a015
89berner <89berner@gmail.com>
2014-10-19 20:11:21 -0300

    Update init.sh

--- elasticsearch/init.sh
+++ elasticsearch/init.sh
@@ -1,3 +1,14 @@
+apt-get install openjdk-7-jre-headless
+sudo update-java-alternatives -s java-1.7.0-openjdk-amd64
+
+cd /opt
+
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.2.zip
+unzip elasticsearch-1.2.2.zip
+
+wget http://download.elasticsearch.org/elasticsearch/marvel/marvel-latest.zip
+bin/plugin --url file:/opt/elasticsearch-1.2.2/marvel-latest.zip --install marvel
+
 ./bin/elasticsearch -Xmx13000M -d
 
 #watch -n1 "curl -XGET 'http://localhost:9200/_cluster/health?pretty=true'"
